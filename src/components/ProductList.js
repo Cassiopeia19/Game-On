44ac9c8fd2bbd0a6d6d50543ab9342f00f3d3abf
Cassiopeia19 <cassiopeia5571@gmail.com>
2020-12-18 11:01:43 -0600

    gridview bug fixed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,9 +4,12 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const {filtered_products:products} = useFilterContext();
+  return (
+    <h4>product list</h4>
+  )
+   const {filtered_products:products} = useFilterContext();
 
-  return <GridView products={products}>games list</GridView>
+   return <GridView products={products}>games list</GridView>
 }
 
 export default ProductList
